b33a924a7e26a6943f8dc5c95e03a4bbffbb78d7
Robert Gentleman <rgentlem@fhcrc.org>
2006-10-10 00:53:00 +0000

    removed startup info and did a little more work on the template
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/affyQCReport@20478 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/zzz.R
+++
@@ -1,9 +0,0 @@
-.First.lib <-
-function( libname, pkgname )
-{
-    require( simpleaffy, quietly = TRUE );
-    require( affy,       quietly = TRUE );
-    cat( "Welcome to 'affyQCReport' V 1.6-1\n" );
-    cat( "Further information available at: www.bifix.org\n");
-    cat( "      mailto: craig.parman@bifix.org\n" );
-}
