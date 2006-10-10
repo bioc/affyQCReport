945cb8e49df8ebc3e731b8743f25b6f7a4201b7c
Robert Gentleman <rgentlem@fhcrc.org>
2006-10-10 16:29:05 +0000

    more updates to the automatic qa report generation tools
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/affyQCReport@20487 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/affyQAReport.R
+++ R/affyQAReport.R
@@ -15,13 +15,22 @@
    cd = setwd(outdir)
    on.exit(setwd(cd))
 
-   outfiles = list(sA = "simpleaffy", hist="affyhist", bxp = "affybxp")
+   outfiles = list(sA = "simpleaffy", RNAdeg = "rnadeg", 
+       hist="affyhist", bxp = "affybxp", NUSE="NUSE", RLE="RLE")
 
    outf = lapply(outfiles, function(x) paste(x, output, sep=".") )
 
    numArrays = nrow(phenoData(affyB))
 
    qcStats = qc(affyB)
+  
+   dfout = data.frame(AvBg = avbg(qcStats), ScaleF=sfs(qcStats),
+      PerCPres=percent.present(qcStats))
+   tab1 = xtable(dfout)
+
+   tab2 = xtable(ratios(qcStats))
+
+   
    pdf(file=outf$sA)
    plot(qcStats)
    dev.off()
@@ -34,17 +43,47 @@
    boxplot(affyB)
    dev.off()
 
+   ##RNA degredation plot
+   rnaDeg = AffyRNAdeg(affyB)
+   pdf(file=outf$RNAdeg)
+   plotAffyRNAdeg(rnaDeg, col=1)
+   dev.off()
+
+   ##affyPLM stuff
+  dataPLM = fitPLM(affyB)
+  
+  #Normalized Unscaled Standard Error (NUSE)
+
+  pdf(file=outf$RLE)
+  Mbox(dataPLM, ylim = c(-1, 1), names = NULL, col="lightblue",
+   whisklty=0, staplelty=0)
+  dev.off()
+
+   pdf(file=outf$NUSE)
+   boxplot(dataPLM, ylim = c(0.95, 1.5), names = NULL,
+        outline = FALSE, col="lightblue")
+   dev.off()
+
+   ##write the LaTeX
    texTemp = system.file("Templates/affyQAtemplate.tex",
 		   package="affyQCReport")
 
-   symVals = c(repName=repName, outfiles)
+   #get version numbers and sessionInfo
+   pkVers =  packageDescription("affyQCReport")$Version
+   sessInfo = toLatex(sessionInfo())
+
+   symVals = c(repName=repName, outfiles, TABLE1=tab1, TABLE2=tab2,
+         affQCVersNO= pkVers, sessionInfo=sessInfo )
 
    outFile = file.path(outdir, paste(repName, ".tex", sep=""))
 
    copySubstitute(texTemp, outFile, symbolValues = symVals)
 
+   ##set up call to pdflatex and run it twice for x-refs
    syscall = paste("pdflatex", outFile)
    system(syscall)
+   system(syscall)
+
    return(list(qcStats=qcStats, loc=outdir))   
  }
 
