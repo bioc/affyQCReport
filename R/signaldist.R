89ed25760688db3017166ce2b61c5a693ee2095f
Wolfgang Huber <w.huber@dkfz-heidelberg.de>
2007-02-09 11:34:51 +0000

    fixed typo
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/affyQCReport@22501 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/signaldist.R
+++ R/signaldist.R
@@ -6,7 +6,7 @@
 
 par(mfrow=c(2, 1))
 ArrayIndex = as.character(1:length(sampleNames(object)))
-boxplot(object,names=ArrayIndex,ylab="Log2(Intesity)",xlab="Array Index")
+boxplot(object,names=ArrayIndex,ylab="Log2(Intensity)",xlab="Array Index")
 
 hist(x=object,lt=1:length(ArrayIndex),col=1:length(ArrayIndex),which="both")
 temppar<-par()
